cbcb4945c172cc92d126efabda50e935319230f7
Kindergir <gir.sipovski@gmail.com>
2023-05-22 17:51:00 +0400

    Revert "Temp fix for history (it doesnt work)"
    This reverts commit d5fa107c8c9304435081c88d3816de008f75929c.

--- Altium/FileSorter/ParallelBatchesWriter.cs
+++ Altium/FileSorter/ParallelBatchesWriter.cs
@@ -8,47 +8,27 @@ namespace FileSorter
 {
 	internal class ParallelBatchesWriter
 	{
-		// for always using an insertion sort on batch
-		private const int MaxBatchSize = 5000;
-
-		private List<Line> currentLines = new List<Line>(MaxBatchSize);
 		private volatile int _finishedTasksCount;
 		private int _tasksCount;
-		private int currentTempFileNumber;
-		private string currentTempFileName;
-
-		public HashSet<string> TempFilesNames { get; private set; }
 
 		public ParallelBatchesWriter()
 		{
-			currentTempFileNumber = 0;
-			currentTempFileName = $"temp_{currentTempFileNumber}.txt";
-			TempFilesNames = new HashSet<string>();
-			_tasksCount = 0;
+			_tasksCount = -1;
 		}
 
-		public async Task AddLine(Line line)
+		public async Task AddTask(List<Line> content, string fileName)
 		{
-			currentLines.Add(line);
+			WriteOneBatch(content, fileName).Start();
+		}
 
-			if (currentLines.Count > MaxBatchSize)
-			{
-				var fullFileName = Path.Combine(Directory.GetCurrentDirectory(), currentTempFileName);
-				TempFilesNames.Add(fullFileName);
-
-				var linesCopy = currentLines;
-				WriteOneBatch(linesCopy, fullFileName).Start();
-
-				currentLines = new List<Line>(MaxBatchSize);
-				++currentTempFileNumber;
-				++_tasksCount;
-				currentTempFileName = $"temp_{currentTempFileNumber}.txt";
-			}
+		public void SetTasksCount(int tasksCount)
+		{
+			_tasksCount = tasksCount;
 		}
 
 		public bool AllTasksCompleted()
 		{
-			return _tasksCount != 0 && _tasksCount == _finishedTasksCount;
+			return _tasksCount != -1 && _tasksCount == _finishedTasksCount;
 		}
 
 		private async Task WriteOneBatch(List<Line> content, string fileName)
