e5bc74f2ef08565decc5ea37cff507ee64070d1e
Kindergir <gir.sipovski@gmail.com>
2023-05-22 21:13:20 +0400

    Temporary state: fast reading and separating by bytes batches. But merger doesnt fixed

--- Altium/FileSorter/FileSeparator.cs
+++ Altium/FileSorter/FileSeparator.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.IO.MemoryMappedFiles;
 using System.Threading;
 
 namespace FileSorter
@@ -16,18 +16,60 @@ namespace FileSorter
 
 		public static HashSet<string> SeparateFile(string fileNameWithPath)
 		{
-			using var fileStream = File.OpenRead(fileNameWithPath);
-			using var streamReader = new StreamReader(fileStream, Encoding.UTF8, false, BufferSize);
+			int batchLength = 1000000; // 1000 kilobytes
+			long batchesCount = 0;
+			using (var fileStream = File.OpenRead(fileNameWithPath))
+			{
+				batchesCount = fileStream.Length / batchLength;
+				if (fileStream.Length % batchLength != 0)
+				{
+					batchesCount += 1;
+				}
+			}
+			//using var streamReader = new StreamReader(fileStream, Encoding.UTF8, false, BufferSize);
 
 			var currentTempFileNumber = 0;
 			var currentTempFileName = $"temp_{currentTempFileNumber}.txt";
 			var tempFilesNames = new HashSet<string>();
 
-			var batchesWriter = new ParallelBatchesWriter();
+			// var batchesWriter = new ParallelBatchesWriter();
+			//
+			// var batch = new List<Line>();
+			// var previousLine = new Line();
 
-			var batch = new List<Line>();
-			var previousLine = new Line();
-			while (streamReader.ReadLine() is { } line)
+
+			var fifFileSorter = new FifFileSorter(batchesCount);
+
+			var currentPosition = 0;
+			using var mmf = MemoryMappedFile.CreateFromFile(fileNameWithPath, FileMode.Open, "ImgA");
+			for (int i = 0; i < batchesCount; i++)
+			{
+				var currentBatch = new byte [batchLength];
+				mmf.CreateViewAccessor().ReadArray(currentPosition, currentBatch, 0, batchLength);
+
+				currentTempFileName = $"temp_{currentTempFileNumber}.txt";
+				var fullFileName = Path.Combine(Directory.GetCurrentDirectory(), currentTempFileName);
+				tempFilesNames.Add(fullFileName);
+
+				fifFileSorter.SortOneFile(fullFileName);
+
+				// var currentTempFile = MemoryMappedFile.CreateFromFile(fullFileName, batchLength, MemoryMappedFileAccess.ReadWrite);
+				var currentTempFile = MemoryMappedFile.CreateFromFile(
+					fullFileName,
+					FileMode.OpenOrCreate,
+					$"bla{currentTempFileNumber}",
+					batchLength,
+					MemoryMappedFileAccess.ReadWrite);
+				
+				currentTempFile.CreateViewAccessor().WriteArray(0, currentBatch, 0, batchLength);
+
+				++currentTempFileNumber;
+				currentPosition += batchLength;
+			}
+
+			SpinWait.SpinUntil(() => fifFileSorter.AllTasksCompleted(), TimeSpan.FromMilliseconds(15));
+
+			/*while (streamReader.ReadLine() is { } line)
 			{
 				var parsedLine = new Line(line);
 				batch.Add(parsedLine);
@@ -51,7 +93,7 @@ namespace FileSorter
 			}
 
 			batchesWriter.SetTasksCount(tempFilesNames.Count);
-			SpinWait.SpinUntil(() => batchesWriter.AllTasksCompleted(), TimeSpan.FromMilliseconds(15));
+			SpinWait.SpinUntil(() => batchesWriter.AllTasksCompleted(), TimeSpan.FromMilliseconds(15));*/
 			return tempFilesNames;
 		}
 	}
--- Altium/FileSorter/ParallelBatchesWriter.cs
+++ Altium/FileSorter/ParallelBatchesWriter.cs
@@ -6,6 +6,52 @@ using System.Threading.Tasks;
 
 namespace FileSorter
 {
+	internal class FifFileSorter
+	{
+		private volatile int _finishedTasksCount;
+		private readonly long _tasksCount;
+
+		public FifFileSorter(long tasksCount)
+		{
+			_tasksCount = tasksCount;
+		}
+
+		public async Task SortOneFile(string fileName)
+		{
+			using var fileStream = File.OpenRead(fileName);
+			using var streamReader = new StreamReader(fileStream, Encoding.UTF8, false, 128);
+
+			var batch = new List<Line>();
+			while (streamReader.ReadLine() is { } line)
+			{
+				var parsedLine = new Line(line);
+				batch.Add(parsedLine);
+			}
+
+			RewriteTemporaryFile(batch, fileName);
+
+			Interlocked.Increment(ref _finishedTasksCount);
+		}
+
+		private static void RewriteTemporaryFile(List<Line> lines, string fileName)
+		{
+			File.Delete(fileName);
+			using var stream = File.Create(fileName);
+			using var writer = new StreamWriter(stream, Encoding.UTF8);
+			foreach (var line in lines)
+			{
+				// TODO encoding
+				writer.WriteLine(line.OriginalValue);
+			}
+			writer.Flush();
+		}
+
+		public bool AllTasksCompleted()
+		{
+			return _tasksCount != -1 && _tasksCount == _finishedTasksCount;
+		}
+	}
+
 	internal class ParallelBatchesWriter
 	{
 		private volatile int _finishedTasksCount;
